a0cebf966cb196479279de370416abca7337bad4
RuRuuux <cbenoit@guardiaschool.fr>
2024-12-17 09:17:39 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const navBar = document.querySelector("header .nav-bar");
-
-hamburger.addEventListener("click", () => {
-    navBar.classList.toggle("active");
-});
